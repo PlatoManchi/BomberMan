70ff5b1ce1caff0960614112f6a925ff608786b6
Plato Manchi <plato.manchi@gmail.com>
2018-01-10 22:28:14 -0500

    Rearranged default assets into folders and renamed files with prefixes
    Rearranged all art into Art folder with sub division for Geometry and Characters
    Renamed all static meshes with SM_ prefix, materials with M_ prefix and blueprints with BP_ prefix

--- Source/BomberMan/BomberManGameMode.cpp
+++ Source/BomberMan/BomberManGameMode.cpp
@@ -7,7 +7,7 @@
 ABomberManGameMode::ABomberManGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Characters/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
