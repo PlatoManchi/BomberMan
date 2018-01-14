a10c27ffc1f16c52084391ade5b9ff3b03cd6f98
Plato Manchi <plato.manchi@gmail.com>
2018-01-14 03:29:19 -0500

    Added SprintPickup

---
+++ Source/BomberMan/Gameplay/Pickups/SprintPickup.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SprintPickup.h"
+#include "Characters/BomberManCharacter.h"
+
+ASprintPickup::ASprintPickup() : 
+	SpeedMultiplier(2.0f)
+{
+}
+
+void ASprintPickup::PickedUpBy(ABomberManCharacter* Character)
+{
+	
+}
---
+++ Source/BomberMan/Gameplay/Pickups/SprintPickup.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Gameplay/Pickups/BasePickup.h"
+#include "SprintPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMAN_API ASprintPickup : public ABasePickup
+{
+	GENERATED_BODY()
+	
+public:
+	/** Default constructor
+	*/
+	ASprintPickup();
+
+protected:
+	/** Called when this pick up is picked by player.
+	*/
+	virtual void PickedUpBy(ABomberManCharacter* Character) override;
+
+private:
+	/** Factor at which 
+	*/
+	UPROPERTY(EditDefaultsOnly, Category = "Pickup|SprintPickup")
+	float SpeedMultiplier;
+};
